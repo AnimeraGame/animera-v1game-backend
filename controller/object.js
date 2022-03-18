a6711932f913b7cb475c8176138d14d5ca582672
Jinge <100747392+sarlongda514@users.noreply.github.com>
2022-03-18 22:41:59 +0700

    Add gltf parser

--- controller/object.js
+++ controller/object.js
@@ -1,9 +1,8 @@
 const upload = require('../middleware/fileFunction.js')('user');
 const objectValidator = require("../validator/object.js");
-const APIRef = 'User Auth';
+const APIRef = 'Object';
 const constants = require("../util/constants");
 const responses = require("../util/responses");
-const geoIP = require('geoip-lite');
 
 const {
     md5,
@@ -91,6 +90,59 @@ routes.get('/get_object_nearby', objectValidator.getObjectNearby, async (req, re
         return responses.sendError(res, languageCode, {}, "", 0);
     }
 
+});
+
+// Add object records to database by using game.json
+routes.get('/add_object_json', async (req, res) => {
+    const languageCode = req.query.language;
+    let json_file = req.query.json ? req.query.json : 'game.json';
+    const games = require("../gltf/" + json_file);
+
+    try {
+        let length = games.length;
+        for (let i=0; i<length; i++){
+            let game = games[i];
+            let object = {};
+            object.entity_id = game.Entity_ID;
+            object.entity_name = game.Entity_Name;
+            object.position_x = game.Position.x;
+            object.position_y = game.Position.y;
+            object.position_z = game.Position.z;
+            object.rotation_x = game.Rotation.x;
+            object.rotation_y = game.Rotation.y;
+            object.rotation_z = game.Rotation.z;
+            object.rotation_w = game.Rotation.w;
+            object.parent_id = game.Parent_ID;
+            object.scale_x = game.Scale.x;
+            object.scale_y = game.Scale.y;
+            object.scale_z = game.Scale.z;
+            object.shape = game.Shape;
+            object.with_collisions = game.withCollisions;
+            object.gltf_path = game.GLTF_Path;
+            object.gltf_rotation_x = game.GLTF_Rotation ? game.GLTF_Rotation.x : null;
+            object.gltf_rotation_y = game.GLTF_Rotation ? game.GLTF_Rotation.y : null;
+            object.gltf_rotation_z = game.GLTF_Rotation ? game.GLTF_Rotation.z : null;
+            object.gltf_rotation_w = game.GLTF_Rotation ? game.GLTF_Rotation.w : null;
+            object.text_string = game.Text_string;
+            object.text_color_r = game.Text_Color ? game.Text_Color.r : null;
+            object.text_color_g = game.Text_Color ? game.Text_Color.g : null;
+            object.text_color_b = game.Text_Color ? game.Text_Color.b : null;
+            object.text_color_a = game.Text_Color ? game.Text_Color.a : null;
+            object.text_width = game.Text_width;
+            object.text_height = game.Text_height;
+            object.text_size = game.Text_fontSize;
+            object.audio_path = game.Audio_Path;
+            object.audio_playing = game.playing;
+            object.audio_loop = game.loop;
+            object.audio_volume = game.volume;
+            object.audio_pitch = game.pitch;
+            await commonFunctions.insertSingleRowIgnore(APIRef, 'object', object, 'Insert object');
+        }
+        return responses.actionCompleteResponse(res, languageCode, {}, "ACTION_COMPLETE", constants.responseMessageCode.ACTION_COMPLETE);
+    } catch (error) {
+        console.log(error);
+        return responses.sendError(res, languageCode, {}, "", 0);
+    }
 });
 
 
