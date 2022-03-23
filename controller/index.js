3f59ede8c975e162c8893af2ef8a0a26424a4ea1
Jinge <100747392+sarlongda514@users.noreply.github.com>
2022-03-23 10:32:06 +0700

    Add health checker

--- controller/index.js
+++ controller/index.js
@@ -6,10 +6,12 @@ module.exports = () => {
     const userAuth = require('./userAuth.js');
     const admin = require('./admin.js');
     const object = require('./object.js');
+    const apis = require('./api.js');
 
     app.use('/user_auth', userAuth);
     app.use('/admin', admin);
     app.use('/object', object);
+    app.use('/api', apis);
 
     return app;
 }
